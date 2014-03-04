51c831638087685e4b4a9bbe00555711d1e2b321
Mateus Caruccio <mateus.caruccio@getupcloud.com>
2014-03-04 19:11:17 -0300

    Ported to python

--- README.md
+++ README.md
@@ -1,86 +1,40 @@
-# oo-index
+Flask on OpenShift
+==================
 
-This package is a general-purpose stand-alone [index](http://app-shifter.rhcloud.com) of [quickstarts](https://www.openshift.com/developers/extend) and [cartridges](https://www.openshift.com/developers/technologies/) for the [OpenShift hosting platform](http://openshift.github.io). 
+This git repository helps you get up and running quickly w/ a Flask installation
+on OpenShift.
 
-This repository contains two major components: 
 
-1. A basic user interface for browsing, searching, and launching open cloud compatible applications and services, built with help from [Angular-js and Twitter bootstrap](http://angular-ui.github.io/bootstrap/).
-2. Default content for [the index](http://app-shifter.rhcloud.com), which is loaded from the included [`quickstart.json` file](https://github.com/openshift/oo-index/blob/master/quickstart.json).
+Running on OpenShift
+----------------------------
 
-## Host your own index
+Create an account at http://openshift.redhat.com/
 
-You can [spin up your own hosted instance of this project on OpenShift Online](https://openshift.redhat.com/app/console/application_types/custom?name=index&initial_git_url=https%3A%2F%2Fgithub.com/openshift/oo-index.git&cartridges[]=nodejs-0.10) in a single click, or use the [rhc command line tool](https://www.openshift.com/get-started#cli) to help configure your local development environment and your OpenShift-hosted environment in a single step:
+Create a python application
 
-    rhc app create index nodejs-0.10 --from-code=https://github.com/openshift/oo-index.git
+    rhc app create flask python-2.6
 
-### Local Development
+Add this upstream flask repo
 
-First, make sure that your npm dependencies are available:
-
-    npm install
+    cd flask
+    git remote add upstream -m master https://github.com/openshift/flask-example.git
+    git pull -s recursive -X theirs upstream master
     
-Then, start a local web server:
-    
-    npm start
+Then push the repo upstream
 
-The project source can be re-bundled by running:
+    git push
 
-    npm run build
-    
-Additional scripts for assisting with development work are [defined in the project's `package.json` file](https://github.com/openshift/oo-index/blob/master/package.json#L23).
+That's it, you can now checkout your application at:
 
+    http://flask-$yournamespace.rhcloud.com
 
-### Data Specification
+------------------------------
 
-Every entry in the `quickstart.json` file should include the following attributes:
+To get more log messages in your openshift logs please add the following line to your code
 
-    {
-      "name": "Ghost",
-      "default_app_name": 'ghost',
-      "git_repo_url": "https://github.com/openshift-quickstart/openshift-ghost-quickstart.git",
-      "cartridges": ["nodejs-0.10"],
-      "website": "http://tryghost.org/",
-      "version": "0.4",
-      "env_variables": {"name1":"value1","name2":"value2"},
-      "tags": ["node.js","ghost","blog"],
-      "description": "Ghost is a free, open, simple blogging platform that's available to anyone who wants to use it"
-    }
+app.config['PROPAGATE_EXCEPTIONS'] = True  
 
-And should comply with the following data format guidelines:
+To read more about logging in Flask please see this email
 
-* **name** - a human-readable name for this project, service, or application (required)
-* **default_app_name** - a short name suggestion, for use in the hosted application url. No hyphens, spaces, or other special characters allowed (a-zA-Z0-9 only, optional) 
-* **git_repo_url** - a URL that points to the project's source code (required)
-* **cartridges** - csv list of strings, starting with the base cartridge type, and continuing with any additional cartridge-based dependencies (required for quickstart applications)
-* **website** - URL pointing to a project homepage (optional)
-* **demo_url** - A link to a live demo (optional)
-* **version** - The release version for this project, usually a numeric string (optional)
-* **env_variables** - a hash of name, value pairs that can be used to intialize the application (optional)
-* **tags** - a csv list of relevant terms and labels (optional)
-* **description** - a basic project description in plain text, with quotes escaped (optional)
+http://flask.pocoo.org/mailinglist/archive/2012/1/27/catching-exceptions-from-flask/
 
-Pull requests that don't meet this criteria will be rejected.
-
-## Contribute via Pull Request
-
-1. Fork the upstream project repository at: [https://github.com/openshift/oo-index](https://github.com/openshift/oo-index)
-2. Add an openshift-compatible service or application to the project's `quickstarts.json` file, making sure to include each of the [required fields](#data-specification).
-3. **Add** and **Commit** your changes locally:
-
-        git add quickstart.json
-        git commit -m 'Adding a Wordpress application to the project index'
-    
-4. **Push** your changes to GitHub:
-
-        git push
-    
-5. Then, [send us a Pull Request](https://github.com/openshift/oo-index/pulls) and we'll update [our hosted index](http://app-shifter.rhcloud.com)
-
-#### Copyright
-
-OpenShift Origin, except where otherwise noted, is released under the Apache License 2.0. See the LICENSE file located in each component directory. 
-
-
-
-+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/dmueller2001/oo-index/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
- +
---
+++ wsgi/myflaskapp.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+import json
+from flask import Flask, g, request, session, url_for, redirect, flash, render_template
+from flask.ext.github import GitHub as AuthGitHub
+import github as PyGitHub
+from collections import OrderedDict
+import requests
+
+class OOIndexError(Exception):
+	'''OO-Index specific errors
+	'''
+
+app = Flask(__name__)
+app.debug = True
+
+try:
+	app.config['GITHUB_CLIENT_ID'] = os.environ['GITHUB_CLIENT_ID']
+	app.config['GITHUB_CLIENT_SECRET'] = os.environ['GITHUB_CLIENT_SECRET']
+	app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'change-me')
+except KeyError, ex:
+	app.config['GITHUB_CLIENT_ID'] = 'FAKE-CLIENT-ID'
+	app.config['GITHUB_CLIENT_SECRET'] = 'FAKE-CLIENT-SECRET'
+	print >>sys.stderr, "Missing config: %s (Please fix)" % ex
+
+try:
+	app.config['GITHUB_CALLBACK_URL'] = 'https://' + os.environ['OPENSHIFT_APP_DNS'] + '/login/callback'
+except KeyError:
+	app.config['GITHUB_CALLBACK_URL'] = 'http://localhost:5000' + '/login/callback'
+
+# set it to point to your own oo-index public repo
+app.config['OO_INDEX_GITHUB_USERNAME'] = os.environ.get('OO_INDEX_GITHUB_USERNAME', 'openshift')
+app.config['OO_INDEX_GITHUB_REPONAME'] = os.environ.get('OO_INDEX_GITHUB_REPONAME', 'oo-index')
+app.config['OO_INDEX_QUICKSTART_JSON'] = os.environ.get('OO_INDEX_QUICKSTART_JSON', 'wsgi/static/quickstart.json').strip('/')
+
+auth = AuthGitHub(app)
+
+## authentication ##########
+
+@app.before_request
+def before_request():
+	try:
+		g.user = session['user']
+	except KeyError:
+		g.user = None
+
+@app.route('/login')
+def login():
+	return auth.authorize(scope='public_repo')
+
+@app.route('/login/callback')
+@auth.authorized_handler
+def authorized(token):
+	next_url = request.args.get('next') or url_for('index')
+	if token is None:
+		return redirect(next_url)
+
+	session['token'] = token
+	session['user']  = auth.get('user')['login']
+	return redirect(next_url)
+
+@auth.access_token_getter
+def token_getter():
+	return session.get('token')
+
+@app.route('/logout')
+def logout():
+	session.pop('user', None)
+	session.pop('token', None)
+	return redirect(url_for('index'))
+
+## views ############
+
+@app.route('/')
+def index():
+	return render_template('index.html')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+	if not g.user:
+		return redirect(url_for('login'))
+
+	pr = None
+	form_data = {}
+	if request.method == 'POST':
+		try:
+			form_data['type'] = request.form['type']
+			form_data['github-username'] = request.form['github-username']
+			form_data['github-repository'] = request.form['github-repository']
+		except KeyError, ex:
+			flash('Missing field: %s' % ex, 'error')
+			return render_template('add.html') #, **form_data)
+		form_data['alternate-name'] = request.form.get('alternate-name')
+		form_data['cartridges'] = request.form.get('cartridges')
+
+		try:
+			pr = send_pull_request(form_data)
+			flash('Pull Request created', 'info')
+		except OOIndexError, ex:
+			flash(str(ex), 'error')
+		except PyGitHub.GithubException, ex:
+			flash(ex.data.get('message', 'Unknown error.'), 'error')
+		except Exception, ex:
+			flash('%s: %s' % (ex.__class__, ex), 'error')
+
+	return render_template('add.html', pr=pr) #, **form_data)
+
+def _get_tree_element(repo, tree, path):
+	for el in tree.tree:
+		if el.path == path:
+			return el
+	raise OOIndexError('Invalid path "%s". Please contact support' % path)
+
+def _read_github_file(username, reponame, filename):
+	'''Fork repo and read content of `filename`.
+	'''
+	print 'Loading file content from %s/%s/%s' % (g.user, reponame, filename)
+	gh = PyGitHub.Github(session['token'])
+	user = gh.get_user()
+
+	# Get user's oo-index repo, create if not exists
+	try:
+		repo = user.get_repo(reponame)
+	except PyGitHub.UnknownObjectException:
+		upstream = '%s/%s' % (username, reponame)
+		print '  Forking project %s' % upstream
+		user.create_fork(gh.get_repo(upstream))
+		repo = None
+
+		for i in range(10):
+			try:
+				repo = user.get_repo(reponame)
+				break
+			except PyGitHub.GithubException:
+				print '  retry %i...' % i
+				time.sleep(2)
+
+		if not repo:
+			msg = 'Timeout creating repository. Please try again later.'
+			flash(msg, 'error')
+			raise OOIndexError(msg)
+
+	head = repo.get_commit('HEAD')
+	tree = repo.get_git_tree(head.sha, recursive=True)
+	blob = _get_tree_element(repo, tree, filename)
+	content = requests.get(blob.url, headers={'Accept': 'application/vnd.github.v3.raw+json'}).json()
+
+	return repo, head, tree, content
+
+def _filter_repo_fields(repo):
+	fields = [
+		'description',
+		'forks',
+		'updated_at',
+		'type',
+		'owner',
+		'id',
+		'size',
+		'watchers',
+		'name',
+		'language',
+		'git_repo_url',
+		'created_at',
+		'default_app_name',
+		'owner_type',
+		'stargazers'
+	]
+
+	r = OrderedDict([ (k,v) for k,v in repo.raw_data.items() if k in fields ])
+	r['stargazers'] = repo.stargazers_count
+	r['owner_type'] = repo.owner.type
+	r['owner'] = repo.owner.html_url
+	return r
+
+def _get_repo_for(username, reponame, token=None):
+	if token:
+		gh = PyGitHub.Github(token)
+	else:
+		gh = PyGitHub.Github()
+	return gh.get_repo(username + '/' + reponame)
+
+def _read_quickstart_repo(username, reponame):
+	print 'Reading quickstart repo metadata %s/%s' % (username, reponame)
+	return _filter_repo_fields(_get_repo_for(username, reponame))
+
+def send_pull_request(form_data):
+	# read metadata of new quickstart repo
+	qs_u = form_data['github-username']
+	qs_r = form_data['github-repository']
+	qs_n = form_data['alternate-name'] or qs_r
+	qs_c = form_data['cartridges'] or []
+	qs_t = form_data['type'] or []
+	try:
+		qs = _read_quickstart_repo(qs_u, qs_r)
+		qs['alternate_name'] = qs_n
+		qs['cartridges'] = qs_c
+		qs['type'] = qs_t
+	except PyGitHub.UnknownObjectException:
+		raise OOIndexError("Username or repository not found: %s/%s" % (qs_u, qs_r))
+
+	# read content of original quickstar.json
+	# fork repo if needed
+	u = app.config['OO_INDEX_GITHUB_USERNAME']
+	r = app.config['OO_INDEX_GITHUB_REPONAME']
+	q = app.config['OO_INDEX_QUICKSTART_JSON']
+	repo, head, tree, quickstart = _read_github_file(u, r, q)
+
+	# add quickstart to quickstart.json
+	quickstart.append(qs)
+
+	# create new blob with updated quickstart.json
+	print "Creating blob...",; sys.stdout.flush()
+	new_blob = repo.create_git_blob(json.dumps(quickstart, indent=3, encoding='utf-8'), 'utf-8')
+
+	# create tree with new blob
+	element = _get_tree_element(repo, tree, q)
+	element = PyGitHub.InputGitTreeElement(path=element.path, mode=element.mode, type=element.type, sha=new_blob.sha)
+
+	if not element:
+		flash("File not found: %s/%s/%s" % (u, r, q), "error")
+		return
+
+	print "Updating tree...",; sys.stdout.flush()
+	new_tree = repo.create_git_tree([ element ], tree)
+
+	# create commit for new tree
+	print "Creating commit...",; sys.stdout.flush()
+	message = 'Quickstart add request: %s/%s' % (qs_u, qs_r)
+	new_commit = repo.create_git_commit(message, new_tree, [ repo.get_git_commit(head.sha) ])
+
+	# create new branch for new commit
+	print "Creating branch...",; sys.stdout.flush()
+	try:
+		new_branch = repo.create_git_ref('refs/heads/%s-%s' % (qs_u, qs_r), new_commit.sha)
+	except PyGitHub.UnknownObjectException:
+		raise OOIndexError("Username or repository not found: %s/%s" % (qs_u, qs_r))
+
+	# and finally, we send our pull request
+	print "Creating pull request...",; sys.stdout.flush()
+	upstream = _get_repo_for(u, r, session['token'])
+	pr_params = {
+		'title': message,
+		'body': 'Automatically generated PR for oo-index',
+		'base': 'master',
+		'head': '%s:%s-%s' % (g.user, qs_u, qs_r),
+	}
+	pr = upstream.create_pull(**pr_params)
+	return pr
+
+##########################
+if __name__ == "__main__":
+	app.run(debug=True)
